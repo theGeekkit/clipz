6e32bd3cc3af97793267e545806d6b50934a4f90
theGeekkit <111545346+theGeekkit@users.noreply.github.com>
2022-12-12 16:30:51 -0600

    adding app-modal 12-12-22

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
+import { AuthModalComponent } from './user/auth-modal/auth-modal.component';
+
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    NavComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        NavComponent,
+        AuthModalComponent
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+
+    ]
 })
 export class AppModule { }
